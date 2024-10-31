99ba2966319348d0d148fba4653c2bbe9af6ca11
Itzmecp <121564783+Cpmodzyt@users.noreply.github.com>
2024-10-31 19:49:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": "",
+  "API_HASH": "2b79fd2d2c83173807a039325e7e166f",
   # Get From my.telegram.org
-  "API_ID": "",
+  "API_ID": "23023343",
   #Get For @BotFather
-  "BOT_TOKEN": "",
+  "BOT_TOKEN": "8188564702:AAHxYGMmAvYFtbZv5Mi0xD0bBPkPMhyV9uA",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "",
+  "DATABASE_URL_PRIMARY": "postgresql://itzmecp_owner:wKDqWiEdQ7c2@ep-bold-voice-a5kb68wc.us-east-2.aws.neon.tech/itzmecp?sslmode=require",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "",
+  "CACHE_CHANNEL": "itzcpbotlogs",
   # Force Subs Channel username without @
-  "CHANNEL": "",
+  "CHANNEL": "Animeforyoulk",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
